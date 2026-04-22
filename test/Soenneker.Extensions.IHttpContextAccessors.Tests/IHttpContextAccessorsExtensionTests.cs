fb68b0b0e3dc1bdd1d292055c9c9c0f4e8a13561
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:58 -0500

    TUnit conversion

--- test/Soenneker.Extensions.IHttpContextAccessors.Tests/IHttpContextAccessorsExtensionTests.cs
+++ test/Soenneker.Extensions.IHttpContextAccessors.Tests/IHttpContextAccessorsExtensionTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Extensions.IHttpContextAccessors.Tests;
 
 public sealed class IHttpContextAccessorsExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
